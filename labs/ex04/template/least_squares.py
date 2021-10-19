e4b57d0c4c699344c491da9d2dfdda91541664b8
AdrianoVM <a.viegasmilani@gmail.com>
2021-10-19 17:13:55 +0200

    finish mandatory stuff

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -6,7 +6,7 @@ Least Square
 
 import numpy as np
 from costs import compute_mse
-
+from labs.ex02.template.costs import *
 
 def least_squares(y, tx):
     """calculate the least squares."""
@@ -16,5 +16,5 @@ def least_squares(y, tx):
     # returns optimal weights, MSE
     # ***************************************************
     w_prime = np.linalg.solve(tx.T @ tx, tx.T @ y)
-    mse = compute_mse(y, tx, w_prime)
+    mse = compute_loss(y, tx, w_prime)
     return w_prime, mse
