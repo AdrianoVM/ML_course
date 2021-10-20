607bbb506d35781d49a1bee4361a3e71c94810b3
AdrianoVM <a.viegasmilani@gmail.com>
2021-10-20 23:21:14 +0200

    start of 5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -9,9 +9,26 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
+
+    w_prime = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    mse = compute_loss(y, tx, w_prime)
+    return w_prime, mse
+
+
+def compute_loss(y, tx, w, mae=False):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
     # ***************************************************
     # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
     # ***************************************************
-    raise NotImplementedError
+    N = tx.shape[0]
+    if mae:
+        print(w)
+        e = y - tx @ np.asarray(w)
+        loss = np.sum(abs(e)) / (2 * N)
+    else:
+        e = y - tx @ np.asarray(w)
+        loss = (e @ e.T) / (2 * N)
+    return loss
